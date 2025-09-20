a27685caf21ef3d9e9b7450b6432519d53ec9f8b
Khvan Nikita <khvan.nik@gmail.com>
2025-09-20 16:50:58 +0900

    fix bug

--- backend/src/email/env.js
+++ backend/src/email/env.js
@@ -7,6 +7,7 @@ export const ENV = {
     CLIENT_URL: process.env.CLIENT_URL,
     RESEND_API_KEY: process.env.RESEND_API_KEY,
     EMAIL_FROM: process.env.EMAIL_FROM,
-    EMAIL_FROM_NAME: process.env.EMAIL_FROM_NAME
+    EMAIL_FROM_NAME: process.env.EMAIL_FROM_NAME,
+    NODE_ENV: process.env.NODE_ENV
 
 }
