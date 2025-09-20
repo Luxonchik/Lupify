11e1652434494d232f54533f75f12508f9c79127
Khvan Nikita <khvan.nik@gmail.com>
2025-09-20 12:56:24 +0900

    signup control

---
+++ backend/src/lib/db.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+
+export const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URL)
+        console.log("MongoDB Connected: ", conn.connection.host)
+    } catch (error) {
+        console.log("Error connection to MongoDB: ", error)
+        process.exit(1) // 1 status code means fail, 0 means success
+
+    }
+ }
