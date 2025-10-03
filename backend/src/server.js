a18b8f37c65e5a76a9203c1d68f1d54778278afb
Khvan Nikita <khvan.nik@gmail.com>
2025-10-03 20:25:02 +0900

    finish project

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,13 @@
-import express from 'express'
+import express from "express";
 
-import path from 'path'
-import authRoutes from "./routes/auth.route.js"
-import messageRoutes from './routes/message.route.js'
-import cookieParser from 'cookie-parser'
-import { connectDB } from './lib/db.js'
-import { ENV } from './email/env.js'
-import cors from 'cors'
-
-
-const app = express()
+import path from "path";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import cookieParser from "cookie-parser";
+import { connectDB } from "./lib/db.js";
+import { ENV } from "./email/env.js";
+import cors from "cors";
+import { app, server } from "./lib/socket.js";
 
 
 const __dirname = path.resolve();
@@ -17,22 +15,20 @@ const __dirname = path.resolve();
 const PORT = ENV.PORT || 3000;
 
 // req.body
-app.use(express.json())
-app.use(cookieParser())
-app.use(cors({origin:ENV.CLIENT_URL, credentials:true}))
+app.use(express.json({ limit: "5mb" }));
+app.use(cookieParser());
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
 
-app.use("/api/auth", authRoutes)
-app.use("/api/messages", messageRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
-
-if(ENV.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")))
-    app.get("*", (req,res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
-    })
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
 
-app.listen(PORT, () => {
-    console.log("Server run port: " + PORT),
-    connectDB()
-})
+server.listen(PORT, () => {
+  console.log("Server run port: " + PORT), connectDB();
+});
